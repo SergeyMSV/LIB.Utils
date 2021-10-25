aed2cdafb2093584ac6a3beb203be759be40d9fd
Sergei <maslennikovserge@yandex.ru>
2021-10-25 21:49:18 +0300

    utilsBase: add. enum class tGNSSCode, tExitCode inside namespace utils
    tExitCode from sysexits.h

--- LIB.Utils/utilsBase.h
+++ LIB.Utils/utilsBase.h
@@ -191,4 +191,39 @@ protected:
 
 //char FromBCD(char dataBCD); [TBD]
 //char ToBCD(char dataBCD); [TBD]
+
+enum class tGNSSCode : std::uint8_t
+{
+	Unknown = 0,
+	GLONASS,
+	GPS,
+	GALILEO,
+	BEIDOU,
+	WAAS = 20,
+	//END = 31, in this case it's possible to use this GNSSCode as an index for a bitfield when navigation solution based on two or more GNSS systems
+};
+
+//from /usr/include/sysexits.h
+enum class tExitCode : int
+{
+	EX_OK = 0,			// successful termination
+	EX__BASE = 64,		// base value for error messages
+	EX_USAGE = 64,		// command line usage error
+	EX_DATAERR = 65,	// data format error
+	EX_NOINPUT = 66,	// cannot open input
+	EX_NOUSER = 67,		// addressee unknown
+	EX_NOHOST = 68,		// host name unknown
+	EX_UNAVAILABLE = 69,// service unavailable
+	EX_SOFTWARE = 70,	// internal software error
+	EX_OSERR = 71,		// system error (e.g., can't fork)
+	EX_OSFILE = 72,		// critical OS file missing
+	EX_CANTCREAT = 73,	// can't create (user) output file
+	EX_IOERR = 74,		// input/output error
+	EX_TEMPFAIL = 75,	// temp failure; user is invited to retry
+	EX_PROTOCOL = 76,	// remote error in protocol
+	EX_NOPERM = 77,		// permission denied
+	EX_CONFIG = 78,		// configuration error
+	EX__MAX = 78,		// maximum listed value
+};
+
 }
