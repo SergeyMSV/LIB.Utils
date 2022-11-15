ce50eed9a953030c52176000cc20a8ab40404d8d
Sergei <maslennikovserge@yandex.ru>
2022-11-15 20:15:14 +0300

    utilsChrono: add. tTimePeriod

--- LIB.Utils/utilsChrono_Test.cpp
+++ LIB.Utils/utilsChrono_Test.cpp
@@ -2,6 +2,7 @@
 
 #include "utilsTest.h"
 
+#include <iomanip>
 #include <iostream>
 #include <thread>
 
@@ -30,6 +31,76 @@ void UnitTest_Chrono()
 		auto Duration = GetDuration<ttime_ms>(tClock::now(), TimeStart);
 
 		test::RESULT("GetDuration 2", Duration == 0);
+	}
+
+	std::cout << "\n""utils::Chrono::tTimePeriod" << std::endl;
+
+	{
+		tTimePeriod TimePeriod(true);
+		TimePeriod.Set(2, true);
+
+		std::time_t t = std::time(nullptr);
+		std::cout << 0 << " > " << std::put_time(std::gmtime(&t), "%T") << '\n';
+
+		for (int i = 0, j = 0; i < 1000; ++i)
+		{
+			if (!TimePeriod.IsReady())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				continue;
+			}
+
+			t = std::time(nullptr);
+			std::cout << ++j << " > " << std::put_time(std::gmtime(&t), "%T") << '\n';
+		}
+	}
+
+	std::cout << "\n""utils::Chrono::tTimePeriodCount" << std::endl;
+
+	{
+		tTimePeriodCount TimePeriod(true);
+		TimePeriod.Set(15, 2, 3, true);
+
+		std::time_t t = std::time(nullptr);
+		std::cout << 0 << " > " << std::put_time(std::gmtime(&t), "%T") << '\n';
+
+		for (int i = 0, j = 0; i < 3000; ++i)
+		{
+			if (!TimePeriod.IsReady())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				continue;
+			}
+
+			t = std::time(nullptr);
+			std::cout << ++j << " > " << std::put_time(std::gmtime(&t), "%T") << '\n';
+		}
+	}
+
+	{
+		tTimePeriodCount TimePeriod(true);
+		TimePeriod.Set(15, 2, 3, true);
+
+		std::time_t t = std::time(nullptr);
+		std::cout << 0 << " > " << std::put_time(std::gmtime(&t), "%T") << '\n';
+
+		for (int i = 0, j = 0; i < 3000; ++i)
+		{
+			if (!TimePeriod.IsReady())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				continue;
+			}
+
+			t = std::time(nullptr);
+			std::cout << ++j << " > " << std::put_time(std::gmtime(&t), "%T") << '\n';
+
+			if (j == 2)
+			{
+				j = 0;
+				TimePeriod.Complete();
+			}
+		}
 	}
 
 
