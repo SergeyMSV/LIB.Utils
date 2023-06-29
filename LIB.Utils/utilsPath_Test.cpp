114562bfc5242eaf8ba22e01d87a0426c105cb7c
Sergei <maslennikovserge@yandex.ru>
2023-06-30 01:54:31 +0300

    utilsPath: removed functions that are put into utilsFile

--- LIB.Utils/utilsPath_Test.cpp
+++ LIB.Utils/utilsPath_Test.cpp
@@ -25,36 +25,6 @@ void UnitTest_Path()
 		utils::test::RESULT("DateTime Parse", memcmp(DateTime1, &DateTime2, sizeof(tm)));
 	}
 
-	{
-		std::filesystem::create_directories("test_root_fs/tmpram");
-		std::ofstream{ "test_root_fs/tmpram/cam_2022-08-02_12-11.jpg" };
-		std::ofstream{ "test_root_fs/tmpram/cam_2022-08-02_13-11.jpg" };
-		std::ofstream{ "test_root_fs/tmpram/cam_2022-08-02_14-11.jpg" };
-		std::ofstream{"test_root_fs/tmpram/temp_file.txt" };
-
-		std::deque<std::string> List = utils::GetFilesLatest("test_root_fs/tmpram", "cam_", 2);
-		utils::test::RESULT("GetFilesList", List.size() == 2 &&
-			List[0] == "test_root_fs/tmpram\\cam_2022-08-02_13-11.jpg" &&
-			List[1] == "test_root_fs/tmpram\\cam_2022-08-02_14-11.jpg");
-
-		std::filesystem::remove_all("test_root_fs/tmpram");
-	}
-
-	{
-		std::filesystem::create_directories("test_root_fs/tmpram");
-		std::ofstream{ "test_root_fs/tmpram/cam_2022-08-02_12-11.jpg" };
-		std::ofstream{ "test_root_fs/tmpram/cam_2022-08-02_13-11.jpg" };
-		std::ofstream{ "test_root_fs/tmpram/cam_2022-08-02_14-11.jpg" };
-		std::ofstream{ "test_root_fs/tmpram/temp_file.txt" };
-
-		RemoveFilesOutdated("test_root_fs/tmpram", "cam_", 1);
-		std::deque<std::string> List = utils::GetFilesLatest("test_root_fs/tmpram", "cam_", 2);
-		utils::test::RESULT("RemoveFilesOutdated", List.size() == 1 &&
-			List[0] == "test_root_fs/tmpram\\cam_2022-08-02_14-11.jpg");
-
-		std::filesystem::remove_all("test_root_fs/tmpram");
-	}
-
 	std::cout << std::endl;
 }
 
