7f4adecd8fd1546177344e26c48b52b470ce668b
Sergei <maslennikovserge@yandex.ru>
2023-08-11 20:37:16 +0300

    utilsLinux: removed GetPath...()

--- LIB.Utils/utilsLinux.cpp
+++ LIB.Utils/utilsLinux.cpp
@@ -55,40 +55,9 @@ std::string CmdLine(const std::string& cmd)
 #endif
 }
 
-std::filesystem::path GetPathW(const std::filesystem::path& pathRaw)
-{
-	if (pathRaw.empty())
-		return {};
-	std::error_code ErrCode;//In order to avoid exceptions
-	std::filesystem::path Path;
-#if defined(_WIN32)
-	Path = "test_root_fs";
-#endif
-	//path("foo") / ""      // the result is "foo/" (appends)
-	//path("foo") / "/bar"; // the result is "/bar" (replaces)
-	if (pathRaw.string()[0] == '/')
-	{
-		Path += pathRaw.lexically_normal();
-	}
-#if defined(_WIN32)
-	else if (pathRaw.string()[0] == '~')
-	{
-		std::string PathRawStr = pathRaw.string();
-		PathRawStr.replace(0, 1, "root");
-		Path /= PathRawStr;
-	}
-#endif
-	else
-	{
-		Path /= pathRaw.lexically_normal();
-	}
-
-	return std::filesystem::canonical(Path, ErrCode);
-}
-
 double GetUptime()
 {
-	std::filesystem::path Path = GetPathW("/proc/uptime");
+	std::filesystem::path Path = path::GetPathNormal("/proc/uptime");
 	std::fstream File(Path, std::ios::in);
 	if (!File.good())
 		return {};
@@ -127,7 +96,7 @@ std::string GetUptimeString()
 
 tCpuInfo GetCpuInfo()
 {
-	std::filesystem::path Path = GetPathW("/proc/cpuinfo");
+	std::filesystem::path Path = path::GetPathNormal("/proc/cpuinfo");
 	std::fstream File(Path, std::ios::in);
 	if (!File.good())
 		return {};
@@ -177,98 +146,6 @@ tCpuInfo GetCpuInfo()
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-namespace PathConfig
-{
-
-const std::vector<std::string> g_PathConfig =
-{
-	{"."},
-	{"../etc"},
-	{"/etc"},
-	{"~/"},
-	{"/etc/default"},
-	{"/usr/local/etc"},
-#if defined(_WIN32)
-	{".."}, // $(ProjectDir)
-#endif
-};
-
-static bool TestPathFile(std::filesystem::path path, const std::string& filename, std::filesystem::path& pathFull)
-{
-	path.append(filename);
-#if defined(LIB_UTILS_LINUX_LOG)
-	std::cout << "TestFile: " << path.string() << '\n';
-#endif
-	std::fstream File = std::fstream(path, std::ios::in);
-	if (!File.good())
-		return false;
-	File.close();
-	pathFull = path;
-	return true;
-}
-
-static std::filesystem::path TestPath(const std::filesystem::path& path, std::string filename, bool currPath, bool testDir)
-{
-	std::filesystem::path PathFull;
-
-	// It can find itself. This statement in order to avoid that situation.
-	if (!currPath && TestPathFile(path, filename, PathFull))
-		return PathFull;
-
-	if (TestPathFile(path, "." + filename, PathFull)) // hidden file
-		return PathFull;
-	if (TestPathFile(path, filename + "rc", PathFull))
-		return PathFull;
-	if(TestPathFile(path, "." + filename + "rc", PathFull)) // hidden file
-		return PathFull;
-	if(TestPathFile(path, filename + ".conf", PathFull))
-		return PathFull;
-	if (TestPathFile(path,filename + ".conf.json", PathFull))
-		return PathFull;
-
-	if (!testDir)
-		return {};
-	std::filesystem::path PathDir = path;
-	PathDir.append(filename);
-	return TestPath(PathDir, filename, false, false);
-}
-
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-std::filesystem::path GetPathConfig(const std::string& filename)
-{
-	if (filename.empty())
-		return {};
-
-	for (const auto& i : PathConfig::g_PathConfig)
-	{
-		bool CurrPath = i == ".";
-#if defined(_WIN32)
-		if (!CurrPath)
-			CurrPath = i == ".."; // $(ProjectDir)
-#endif
-		std::filesystem::path PathItem = GetPathW(i);
-		if (PathItem.empty())
-			continue;
-		std::filesystem::path Path = PathConfig::TestPath(PathItem, filename, CurrPath, true);
-		if (!Path.empty())
-			return Path;
-	}
-
-	return {};
-}
-
-std::filesystem::path GetPathConfigExc(const std::string& filename)
-{
-	std::filesystem::path Str = GetPathConfig(filename);
-	if (Str.empty())
-		throw std::runtime_error("File not found: " + filename);
-	return Str;
-};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
 // Functions for tests
 #if defined(_WIN32)
 static std::string CmdLineWinTest(const std::string& cmd)
@@ -284,7 +161,7 @@ Swap:              0           0           0";
 	std::string CmdValue = GetStringEnding("cat", cmd);
 	if (!CmdValue.empty())
 	{
-		std::filesystem::path Path = GetPathW(CmdValue);
+		std::filesystem::path Path = path::GetPathNormal(CmdValue);
 		if (Path.empty())
 			return {};
 
