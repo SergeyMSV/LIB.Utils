30ac36e2c6fa018707522776814ad5385e3f1cc9
Sergei <maslennikovserge@yandex.ru>
2023-08-11 20:33:59 +0300

    utilsPath: GetPathConfig & GetPathConfigEx from utilsLinux

--- LIB.Utils/utilsPath.cpp
+++ LIB.Utils/utilsPath.cpp
@@ -1,5 +1,6 @@
 #include "utilsPath.h"
 
+#include <array>
 #include <filesystem>
 #include <iomanip>
 #include <sstream>
@@ -29,6 +30,9 @@ tm GetDateTime(const std::string& a_value)
 	return DateTime;
 }
 
+namespace path
+{
+
 std::string GetAppName(const std::string& path)
 {
 	const std::filesystem::path Path{ path };
@@ -46,6 +50,120 @@ std::string GetAppNameMain(const std::string& path)
 	if (Pos != std::string::npos)
 		MainPart = MainPart.substr(0, Pos);
 	return MainPart;
+}
+
+std::filesystem::path GetPathW(const std::filesystem::path& pathRaw)
+{
+	if (pathRaw.empty())
+		return {};
+	std::error_code ErrCode;//In order to avoid exceptions
+	std::filesystem::path Path;
+#if defined(_WIN32)
+	Path = "test_root_fs";
+#endif
+	//path("foo") / ""      // the result is "foo/" (appends)
+	//path("foo") / "/bar"; // the result is "/bar" (replaces)
+	if (pathRaw.string()[0] == '/')
+	{
+		Path += pathRaw.lexically_normal();
+	}
+#if defined(_WIN32)
+	else if (pathRaw.string()[0] == '~')
+	{
+		std::string PathRawStr = pathRaw.string();
+		PathRawStr.replace(0, 1, "root");
+		Path /= PathRawStr;
+	}
+#endif
+	else
+	{
+		Path /= pathRaw.lexically_normal();
+	}
+
+	return std::filesystem::canonical(Path, ErrCode);
+}
+
+static std::filesystem::path TestPath(const std::filesystem::path& path, std::string filename, bool currPath, bool testDir)
+{
+	auto TestPathFile = [](std::filesystem::path path, const std::string& filename, std::filesystem::path& pathFull)
+	{
+		path.append(filename);
+#if defined(LIB_UTILS_LINUX_LOG)
+		std::cout << "TestFile: " << path.string() << '\n';
+#endif
+		std::error_code ErrCode;//In order to avoid exceptions
+		pathFull = std::filesystem::canonical(path, ErrCode);
+		return !pathFull.empty();
+	};
+
+	std::filesystem::path PathFull;
+
+	// It can find itself. This statement in order to avoid that situation.
+	if (!currPath && TestPathFile(path, filename, PathFull))
+		return PathFull;
+
+	if (TestPathFile(path, "." + filename, PathFull)) // hidden file
+		return PathFull;
+	if (TestPathFile(path, filename + "rc", PathFull))
+		return PathFull;
+	if (TestPathFile(path, "." + filename + "rc", PathFull)) // hidden file
+		return PathFull;
+	if (TestPathFile(path, filename + ".conf", PathFull))
+		return PathFull;
+	if (TestPathFile(path, filename + ".conf.json", PathFull))
+		return PathFull;
+
+	if (!testDir)
+		return {};
+	std::filesystem::path PathDir = path;
+	PathDir.append(filename);
+	return TestPath(PathDir, filename, false, false);
+}
+
+std::filesystem::path GetPathConfig(const std::string& filename)
+{
+	if (filename.empty())
+		return {};
+
+	constexpr std::array PathConfig =
+	{
+		".",
+		"../etc",
+		"/etc",
+		"~/",
+		"/etc/default",
+		"/usr/local/etc",
+	#if defined(_WIN32)
+		"..", // $(ProjectDir)
+	#endif
+	};
+
+	for (const auto& i : PathConfig)
+	{
+		bool CurrPath = i == ".";
+#if defined(_WIN32)
+		if (!CurrPath)
+			CurrPath = i == ".."; // $(ProjectDir)
+#endif
+		std::filesystem::path PathItem = GetPathW(i);
+		if (PathItem.empty())
+			continue;
+		std::filesystem::path Path = TestPath(PathItem, filename, CurrPath, true);
+		if (!Path.empty())
+			return Path;
+	}
+
+	return {};
+}
+
+std::filesystem::path GetPathConfigExc(const std::string& filename)
+{
+	std::filesystem::path Str = GetPathConfig(filename);
+	if (Str.empty())
+		throw std::runtime_error("File not found: " + filename);
+	return Str;
+}
+
 }
 
 }
