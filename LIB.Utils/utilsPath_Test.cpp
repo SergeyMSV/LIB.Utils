418597e98b1b5eac4ecad60cefdcf4e85aa2bf84
Sergei <maslennikovserge@yandex.ru>
2023-08-11 20:33:59 +0300

    utilsPath: GetPathConfig & GetPathConfigEx from utilsLinux

--- LIB.Utils/utilsPath_Test.cpp
+++ LIB.Utils/utilsPath_Test.cpp
@@ -25,6 +25,43 @@ void UnitTest_Path()
 		utils::test::RESULT("DateTime Parse", memcmp(DateTime1, &DateTime2, sizeof(tm)));
 	}
 
+	std::cout << "\n""utils::linux::GetPathConfig\n";
+
+	{
+		auto Res = path::GetPathConfig("test1");
+		utils::test::RESULT(".conf", Res.filename().string() == "test1.conf");
+	}
+
+	{
+		auto Res = path::GetPathConfig("test2");
+		utils::test::RESULT(".conf.json", Res.filename().string() == "test2.conf.json");
+	}
+
+	{
+		auto Res = path::GetPathConfig("test3");
+		utils::test::RESULT("~/.test3rc", Res.filename().string() == ".test3rc");
+	}
+
+	{
+		auto Res = path::GetPathConfig("test4");
+		utils::test::RESULT("/etc/test4rc", Res.filename().string() == "test4rc");
+	}
+
+	{
+		auto Res = path::GetPathConfig("test5");
+		utils::test::RESULT("/etc/test5", Res.filename().string() == "test5");
+	}
+
+	{
+		auto Res = path::GetPathConfig("test6");
+		utils::test::RESULT("~/.test6", Res.filename().string() == ".test6");
+	}
+
+	{
+		auto Res = path::GetPathConfig("test7win");
+		utils::test::RESULT("test7win", Res.filename().string() == "test7win.conf.json");
+	}
+
 	std::cout << std::endl;
 }
 
