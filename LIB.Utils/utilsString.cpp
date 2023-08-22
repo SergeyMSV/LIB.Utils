4cc2778484511dc89d19022ad2853ba372185d98
Sergei <maslennikovserge@yandex.ru>
2023-08-22 20:37:25 +0300

    utilsString: tolower/toupper

--- LIB.Utils/utilsString.cpp
+++ LIB.Utils/utilsString.cpp
@@ -1,9 +1,23 @@
 #include "utilsString.h"
 
+#include <algorithm>
+
 namespace utils
 {
 	namespace string
 	{
+
+std::string tolower(std::string s)
+{
+	std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
+	return s;
+}
+
+std::string toupper(std::string s)
+{
+	std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::toupper(c); });
+	return s;
+}
 
 std::optional<std::string> Replace(const std::string& str, const std::string& from, const std::string& to)
 {
