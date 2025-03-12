3affbb70ecbaac9ecf25788b8931f85a13cfee7e
Sergei <maslennikovserge@yandex.ru>
2025-03-12 23:21:05 +0300

    utilsBase: removed exception - THROW_INVALID_ARGUMENT, THROW_RUNTIME_ERROR

--- LIB.Utils/utilsBase.h
+++ LIB.Utils/utilsBase.h
@@ -12,26 +12,8 @@
 #include <cstring>
 
 #include <algorithm>
-#include <filesystem>
-#include <stdexcept>
 #include <string>
 #include <vector>
-
-namespace utils
-{
-
-inline std::string GetLogMessage(const std::string& msg, const std::string& filename, int line)
-{
-	const std::filesystem::path Path(filename);
-	return msg + " " + Path.filename().string() + ":" + std::to_string(line);
-}
-
-}
-
-#define THROW_INVALID_ARGUMENT(msg) throw std::invalid_argument{ utils::GetLogMessage(msg, __FILE__, __LINE__) }
-#define THROW_RUNTIME_ERROR(msg) throw std::runtime_error{ utils::GetLogMessage(msg, __FILE__, __LINE__) }
-
-///////////////////////////////////////////////////////////////////////////////
 
 namespace utils
 {
