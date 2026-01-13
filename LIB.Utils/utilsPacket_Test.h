f2277c12157313e4085fe2e9f26021c8bca96df4
Sergei <maslennikovserge@yandex.ru>
2026-01-14 01:08:14 +0300

    utilsPacket & utilsPacketStar

--- LIB.Utils/utilsPacket_Test.h
+++ LIB.Utils/utilsPacket_Test.h
@@ -1,18 +1,18 @@
 //2021-10-29
 #pragma once
 
-#include "utilsBase.h"
 #include "utilsTest.h"
 
 #include <string>
+#include <vector>
 
 namespace utils
 {
 
 template<typename T>
-void UnitTest_Packet_Find(const std::string& testName, const tVectorUInt8& raw, const tVectorUInt8& data)
+void UnitTest_Packet_Find(const std::string& testName, const std::vector<std::uint8_t>& raw, const std::vector<std::uint8_t>& data)
 {
-	tVectorUInt8 DataVector = raw;
+	std::vector<std::uint8_t> DataVector = raw;
 
 	T Packet;
 
@@ -24,7 +24,7 @@ void UnitTest_Packet_Find(const std::string& testName, const tVectorUInt8& raw, 
 	}
 	else
 	{
-		const tVectorUInt8 A = Packet.ToVector();
+		const std::vector<std::uint8_t> A = Packet.ToVector();
 
 		Result = Result &&
 			A == data;
@@ -36,15 +36,15 @@ void UnitTest_Packet_Find(const std::string& testName, const tVectorUInt8& raw, 
 template<typename T>
 void UnitTest_Packet_Find(const std::string& testName, const std::string& raw, const std::string& data)
 {
-	UnitTest_Packet_Find<T>(testName, tVectorUInt8(raw.begin(), raw.end()), tVectorUInt8(data.begin(), data.end()));
+	UnitTest_Packet_Find<T>(testName, std::vector<std::uint8_t>(raw.begin(), raw.end()), std::vector<std::uint8_t>(data.begin(), data.end()));
 }
 
 template<typename T>
-void UnitTest_Packet_Parse(const std::string& testName, const tVectorUInt8& raw, const T& packet)
+void UnitTest_Packet_Parse(const std::string& testName, const std::vector<std::uint8_t>& raw, const T& packet)
 {
 	typename T::payload_value_type checkPacketData = packet.GetPayloadValue();
 
-	tVectorUInt8 DataVector = raw;
+	std::vector<std::uint8_t> DataVector = raw;
 
 	T Packet;
 
@@ -63,13 +63,13 @@ void UnitTest_Packet_Parse(const std::string& testName, const tVectorUInt8& raw,
 template<typename T>
 void UnitTest_Packet_Parse(const std::string& testName, const std::string& raw, const T& packet)
 {
-	UnitTest_Packet_Parse<T>(testName, tVectorUInt8(raw.begin(), raw.end()), packet);
+	UnitTest_Packet_Parse<T>(testName, std::vector<std::uint8_t>(raw.begin(), raw.end()), packet);
 }
 
 template<typename T>
 void UnitTest_Packet_Make(const std::string& testName, const T& packet)
 {
-	const tVectorUInt8 DataVector = packet.ToVector();
+	const std::vector<std::uint8_t> DataVector = packet.ToVector();
 
 	UnitTest_Packet_Parse<T>(testName, DataVector, packet);
 }
