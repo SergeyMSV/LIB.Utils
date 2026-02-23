e2079dae0937be066981b9e7c6c6718bca410268
Sergei <maslennikovserge@yandex.ru>
2026-02-23 20:19:54 +0300

    utilsTest: RESULT_Total(..)

--- LIB.Utils/utilsTest.h
+++ LIB.Utils/utilsTest.h
@@ -5,6 +5,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+#include <libConfig.h>
+
 #include <string>
 
 #include <algorithm>
@@ -20,6 +22,7 @@ namespace utils
 void ASSERT(bool value);
 void RESULT(const char* msg, bool result);
 void RESULT(const std::string& msg, bool result);
+void RESULT_Total();
 void WARNING(const char* msg, bool show = true);
 void WARNING(const std::string& msg, bool show = true);
 
